633d63d04ed23e81baeaae960d2197723b6bc836
charmful0x <77340894+charmful0x@users.noreply.github.com>
2021-01-25 21:45:29 +0200

    Update sendRequest.js

--- src/sendRequest.js
+++ src/sendRequest.js
@@ -2,12 +2,41 @@ async function sendRequest() {
 
     const arweave = Arweave.init();
     const username = document.getElementById("username").value
+    const bio = document.getElementById("bio").value
     const userId = pub_key;
+    
+    
+    // force username length
+    if (username.length > 30 || username.length < 4) {
+        alert("username must be between 4 and 30 characters");
+        return
+    };
 
+
+
+    // replace whitespace with dash in username input
+    let usr = '';
+    for (i = 0; i < username.length; i++) {
+        if (username[i].charCodeAt(0) === 32) {
+            usr += "-"
+        }
+        else {
+            usr += username[i]
+        }
+    };
+
+
+    if (typeof jwk == "undefined") {
+        alert("please import your wallet")
+        return
+     };
+    
+    
     const userObject = {
 
-        "username": username,
+        "username": usr,
         "user_id": userId,
+        "bio": bio,
         "registration_unix_epoch": Date.now()
     };
 
@@ -18,8 +47,8 @@ async function sendRequest() {
             JSON.parse(jwk)
         );
 
-    transaction.addTag("App-Name", "web3gram");
-    transaction.addTag("username", username);
+    transaction.addTag("App-Name", "decent.land");
+    transaction.addTag("username", usr);
     transaction.addTag("version", "0.0.1");
     transaction.addTag("action", "signup");
     transaction.addTag("Content-Type", "application/json");
@@ -28,7 +57,7 @@ async function sendRequest() {
         
     await arweave.transactions.sign(transaction, JSON.parse(jwk));
     await arweave.transactions.post(transaction)
-    alert(transaction.id)
+    alert(`registration txID ${transaction.id}`)
 
 };
 
