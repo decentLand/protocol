239fae5d29b42ac1df1554aae72d5d7f6f6f686e
charmful0x <77340894+charmful0x@users.noreply.github.com>
2021-02-01 23:09:22 +0200

    identicons
    added random profile picture account's binding at registration

--- src/sendRequest.js
+++ src/sendRequest.js
@@ -1,8 +1,20 @@
+const identicons = ["9NIxzk99mjkKVa4_jl9EgncJks5wVlxp_LIvEWDuKyA", "12kZuMT3P-RHkyHNrNeH-12SC9WKnlLz3CitPquTIfU", "pTADN_N_M0rZBAPj0nKlVqatmncOzQ1MGUHMOxnHbc0",
+                    "YIIOVeXyzHu-PKE-9L8fxCWTrLEm32wEn62t_BE7cvs", "t9PxVURNO-TaoMaEAizBzwbSqcq0AmU3uwhPiE4adfY", "UkLaA8RkJGlXp1oDzApvSVZLapYD6_d2X3GBYrZFWHk",
+                    "klWbfS1ZREyb2g-PZ79iJE1UHsFf4hQMKqXg72DCXOw", "ukFDu-nFUeAgFIldtd28m1114udMRti2Vfv0hXY-uwU", "qAgFNVwmRs8L6s8n7_RRDMCzn_V7g634posoQasreRs",
+                    "yJa8hVvmyWdlSYjX2Jn0FcnDJMl7-tbYDejuWVT5r5o", "KePbvEa55KXavT7WNITRflTwRUK42KARqY_Fyd08JJ4", "8PPhRziuLUpokhvoeezIjCGP450ZhHYo5HG5upOeMOI"]
+
+
+function generate_random_pfp() {
+
+    return identicons[Math.floor( Math.random() * identicons.length)]
+};
+
 async function sendRequest() {
 
     const arweave = Arweave.init();
-    const username = document.getElementById("username").value
-    const bio = document.getElementById("bio").value
+    const username = document.getElementById("username").value;
+    const bio = document.getElementById("bio").value;
+    const pfp = generate_random_pfp();
     const userId = pub_key;
     
     
@@ -37,6 +49,7 @@ async function sendRequest() {
         "username": usr,
         "user_id": userId,
         "bio": bio,
+        "pfp": pfp,
         "registration_unix_epoch": Date.now()
     };
 
