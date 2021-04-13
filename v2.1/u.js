5bbd09cb193036ac1c117a5b018a1e824bcfea25
charmful0x <77340894+charmful0x@users.noreply.github.com>
2021-04-13 17:09:53 +0300

    feature: cacheing

--- v2.1/u.js
+++ v2.1/u.js
@@ -189,5 +189,22 @@ async function isArverified(address) {
 
 };
 
+async function getCacheData() {
+    const archiver_addr = "rB86LBc5uSm68ZGh5v2sdiM-8r4uFOZ35VZwBvnesqo"
+    const last_tx = await arweave.wallets.getLastTransactionID(archiver_addr)
+
+    const last_cache_data = await arweave.transactions.getData(last_tx, {decode: true, string: true});
+
+    const cache_object = JSON.parse(last_cache_data);
+    const cache = new Map( Object.entries(cache_object) );
+    const data =  cache.get("null");
+
+    
+    return await arweave.transactions.getData(data, {decode: true, string: true})
+    
+
+    
+};
+
 
 parseProfileData(userAddress)
